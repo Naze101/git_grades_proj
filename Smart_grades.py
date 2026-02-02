a28870319837775c00f930809770108fc01b8758
Naze101 <154392062+Naze101@users.noreply.github.com>
2026-02-02 14:02:12 -0800

    #4 Commit -- new branch + testing of different structure
    I moved to a testing branch. If successful here, I can move everything to main. I have created a list named "grades" which will hold all grades when regex finds numbers from inputs.
    
    I have also added logic into finding the average grade with loop logic, highest and lowest grades.
    
    Next steps are testing and putting average into letter grade

--- Smart_grades.py
+++ Smart_grades.py
@@ -10,14 +10,27 @@
 import re
 # create function that does the following:
 def grade_analysis(): 
+
+    grades = [] # variable that will contain all grades
     # declare input
     user_grades = input("Please input the grades you recieved")
     # use regex to clean input to get respect grades
-    db_grades = re.findall("[0-9][0-9]", user_grades)
-    s_grades = re.findall("(?<!\d)\d(?=%)", user_grades) # "[0-9]"= X, ?![a-zA-Z0-9])[0-9]% = X. 
-    hundred_grades = re.findall("100", user_grades)
+    db_grades = re.findall("[0-9][0-9]", grades)
+    s_grades = re.findall("(?<!\d)\d(?=%)", grades) # "[0-9]"= X, ?![a-zA-Z0-9])[0-9]% = X. 
+    hundred_grades = re.findall("100", grades)
     # use numbers in list, add them up, and calculate the average "for num in list, total_num + num" and then calculate by dividing by amount of numbers
     #Also find highest and lowest using min() and max() function
+    #variable that numbers are collected into
+    total_num = 0
+    subj_num = len(grades)
+    
+    for num in grades:
+        total_num += int(num)
+    average = total_num/subj_num
+
+    # highest numbers 
+    highest_grade = max(grades)
+    lowest_grades = min(grades)
 
     #Associate calculated average with letter grades
 
