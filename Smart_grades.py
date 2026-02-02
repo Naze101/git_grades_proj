79cf52898144cfb904a7760f897f8a67cd36c459
Naze101 <154392062+Naze101@users.noreply.github.com>
2026-02-01 21:48:03 -0800

    #2 Commit--PsuedoCode
    This commit is filled with psuedocode for how the project will be structured. I have thought out the structure of the function I will be using (including min and max functions and looping through a set).

---
+++ Smart_grades.py
@@ -0,0 +1,23 @@
+# This is a system made to take user inputs of grades and output average (% + Letter grade)
+# We need to be able to strip input of any stuff we do not want
+
+#Plan: Get input from user, strip of jargon using regex, put numbers obtained into a dictionary [English: 98%]
+#Add values in dictionary together and calculate (find number of values based on for loop)
+# Correlate average % to letter grade using if statements. 
+# output in string format
+
+#Declare any libraries needed (regex)
+
+# create function that does the following: 
+    # declare input
+
+    # use regex to clean input to get respect grades
+
+    # use numbers in list, add them up, and calculate the average "for num in list, total_num + num" and then calculate by dividing by amount of numbers
+    #Also find highest and lowest using min() and max() function
+
+    #Associate calculated average with letter grades
+
+    #output grades with letter
+
+
