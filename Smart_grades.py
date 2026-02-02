3476a64fa807b5f12c6e58f67fc4ab268f3aaf05
Naze101 <154392062+Naze101@users.noreply.github.com>
2026-02-02 13:21:59 -0800

    #3 Commit-Regex
    In this Commit, I have successfully worked through Regex patterns needed. I have commented areas where I have tried different patterns so that I have a list of what I have tried and hasn't worked.
    
    I have also outputted numbers obtained in lists for testing.
    
    Next steps are to create loop structure

--- Smart_grades.py
+++ Smart_grades.py
@@ -7,17 +7,24 @@
 # output in string format
 
 #Declare any libraries needed (regex)
-
-# create function that does the following: 
+import re
+# create function that does the following:
+def grade_analysis(): 
     # declare input
-
+    user_grades = input("Please input the grades you recieved")
     # use regex to clean input to get respect grades
-
+    db_grades = re.findall("[0-9][0-9]", user_grades)
+    s_grades = re.findall("(?<!\d)\d(?=%)", user_grades) # "[0-9]"= X, ?![a-zA-Z0-9])[0-9]% = X. 
+    hundred_grades = re.findall("100", user_grades)
     # use numbers in list, add them up, and calculate the average "for num in list, total_num + num" and then calculate by dividing by amount of numbers
     #Also find highest and lowest using min() and max() function
 
     #Associate calculated average with letter grades
 
     #output grades with letter
+    print(hundred_grades)
+    print(db_grades)
+    print(s_grades)
+grade_analysis()
 
 
